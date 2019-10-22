87fab06280b2c40ac462d176dfaa040210d7fddd
mzawlodzki <37063458+mzawlodzki@users.noreply.github.com>
2019-10-21 17:50:14 -0700

    Add files for python homework II

---
+++ number_while_loop.py
@@ -0,0 +1,10 @@
+# create list of numbers and loop through to add 1 to each
+#! /usr/bin/Python3
+
+#create list of numbers
+numlist = list(range(2,11))
+
+#loop through and add 1 to each item on the list
+for num in numlist:
+	print(num + 1)
+		
